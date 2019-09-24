f1946697e1d4e6ad8dd722fbd1c6b7477fa00f59
madhukar <madhukarjeeverma@yahoo.com>
2019-09-24 11:03:02 +0530

    Working on Room Database to set selected option-now database is not crashing

---
+++ app/src/main/java/com/preprepare/prepare/Welcome.java
@@ -0,0 +1,31 @@
+package com.preprepare.prepare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class Welcome extends AppCompatActivity {
+
+    private static final int SPLASH_SCREEEN_TIMEOUT = 1000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(1000);
+                    startActivity(new Intent(Welcome.this, LoginPage.class));
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, SPLASH_SCREEEN_TIMEOUT);
+    }
+}
